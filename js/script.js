8c6d3ad51722655859275f826cc13c9c52cecb27
Maria de los Angeles <maruorellano3@gmail.com>
2020-09-11 17:22:32 -0300

    cambios agregados.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-function jugar(){
-    producto_elegido= "colchon";
-    producto = document.getElementById('prod').value;
-
 
     if (producto != "")
     {
@@ -26,5 +22,39 @@ function jugar(){
 
        
     }   
+    
+function jugar() {
+
+    var cant_intentos = 3;
+    var acerto = false;
+    var producto_elegido= "colchon";
+    var producto;
+
+    var nombre = document.getElementById("nombre").value;
+
+    if (nombre == "") {
+        alert("Por favor, ingrese su nombre para poder jugar");
+    } else {
+        while (cant_intentos > 0 && acerto != true) {
+            cant_intentos--;
+            producto = prompt("Ingrese el producto");
+            if (producto == producto_elegido) {
+                alert(" Felicitaciones ACERTASTE!!!");
+                acerto = true;
+            } else if (cant_intentos == 3) {
+                alert("Te quedan " + cant_intentos + " intentos");
+            } else if (cant_intentos == 2) {
+                alert("PISTA: Se utiliza para descansar. \n  (Te quedan " + cant_intentos + " intentos)");
+            } else if (cant_intentos == 1) {
+                alert("PISTA: Viene en varios tamaños. \n  (Te queda " + cant_intentos + " intento)");
+
+            } else {
+               alert("Lo siento no te quedan mas intentos, no pudo ser.");
+            }
+
+        }
+
+        document.getElementById("prod").innerHTML = "El producto escondido era: <strong>Colchon</strong>."
+    }
+
 }
-            
